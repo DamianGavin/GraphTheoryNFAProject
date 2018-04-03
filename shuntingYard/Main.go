d4e885e0ee0e4e226ac555d26be93bdbe59f29cb
DamianGavin <10007969@gmit.ie>
2018-04-03 11:49:35 +0100

    finished shunting yard comments

--- shuntingYard/Main.go
+++ shuntingYard/Main.go
@@ -17,24 +17,40 @@ func infixToPostfix(infix string) string {
 	var postfix []rune
 
 	//this is the algorithm.
+	//loops through infix and returns index of character.
+	//converts strings to an array of runes.
 	for _, r := range infix {
 		switch {
+		//1st case is open bracket "("
+		//append is a built in function that just adds to the end.
 		case r == '(':
 			s = append(s, r)
 		case r == ')':
+			//closing bracket will pop off the stack until we find the open bracket.
+			//ie. for the last element != "("
 			for s[len(s)-1] != '(' {
 				postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1]
 			}
+			//if the character is a closing bracket keep popping off the stack.
 			s = s[:len(s)-1]
+
+			//this will be true if r is in the special characters array
 		case specials[r] > 0:
+			//while stack still has elements and the precedence of current element
+			//is <= the precedence of the top element of the stack. Take the element off the
+			//stack and put it into postfix.
 			for len(s) > 0 && specials[r] <= specials[s[len(s)-1]] {
 				postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1]
 			}
 			s = append(s, r)
+
+			//default is when r is neither a bracket nor a special character.
 		default:
 			postfix = append(postfix, r)
 		}
 	}
+	//at the end of the process if there is anything left on the stack just
+	//append it onto the output.
 	for len(s) > 0 {
 		postfix, s = append(postfix, s[len(s)-1]), s[:len(s)-1]
 	}
